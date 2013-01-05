18986df00234806a5dac7a458d1fd54c0a20d1a7
Kostya Bazhanov <kostyabazhanov@mail.ru>
2013-01-05 17:38:47 +0400

    BitSet

--- CanonicalBasis/fca_bitset.cpp
+++ CanonicalBasis/fca_bitset.cpp
@@ -1,164 +1,228 @@
 # include "fca_bitset.h"
 
+# include <sstream>
+
 # ifdef BOOST_BITSET_ON
 
-FCA::BitSet::BitSet(const size_t new_size) : m_bs(new_size) {}
+FCA::BitSet::BitSet(const size_t newSize) : mBS(newSize) {}
 
-FCA::BitSet::BitSet(const FCA::BitSet &a) : m_bs(a.m_bs) {}
+FCA::BitSet::BitSet(const FCA::BitSet& a) : mBS(a.mBS) {}
 
 bool FCA::BitSet::none() const
 { 
-    return m_bs.none(); 
+    return mBS.none(); 
 }
 
 bool FCA::BitSet::test(const size_t ind) const 
 { 
-    return m_bs.test(ind);
+    return mBS.test(ind);
 }
 
 void FCA::BitSet::set(const size_t ind) 
 {
-    m_bs.set(ind);
+    mBS.set(ind);
 }
 
 void FCA::BitSet::set() 
 { 
-    m_bs.set();
+    mBS.set();
 }
 
 void FCA::BitSet::reset(const size_t ind)
 { 
-    m_bs.reset(ind);
+    mBS.reset(ind);
 }
 
 void FCA::BitSet::reset() 
 {
-    m_bs.reset();
+    mBS.reset();
 }
 
 void FCA::BitSet::flip() 
 { 
-    m_bs.flip(); 
+    mBS.flip(); 
 }
 
-bool FCA::BitSet::is_subset_of(const FCA::BitSet &a) const 
+bool FCA::BitSet::is_subset_of(const FCA::BitSet& a) const 
 { 
-    return m_bs.is_subset_of(a.m_bs); 
+    return mBS.is_subset_of(a.mBS); 
 }
 
-bool FCA::BitSet::is_proper_subset_of(const FCA::BitSet &a) const
+bool FCA::BitSet::is_proper_subset_of(const FCA::BitSet& a) const
 { 
-    return m_bs.is_proper_subset_of(a.m_bs); 
+    return mBS.is_proper_subset_of(a.mBS); 
 }
 
 size_t FCA::BitSet::size() const 
 { 
-    return m_bs.size();
+    return mBS.size();
 }
 
 void FCA::BitSet::resize(const size_t new_size)
 {
-    return m_bs.resize(new_size);
+    return mBS.resize(new_size);
 }
 
 size_t FCA::BitSet::count() const 
 { 
-    return m_bs.count(); 
+    return mBS.count(); 
 }
 
-FCA::BitSet &FCA::BitSet::operator |=(const FCA::BitSet &a) 
+FCA::BitSet& FCA::BitSet::operator |=(const FCA::BitSet& a) 
 { 
-    m_bs |= a.m_bs; 
+    mBS |= a.mBS; 
     return *this;
 }
 
-FCA::BitSet &FCA::BitSet::operator -=(const FCA::BitSet &a) 
+FCA::BitSet& FCA::BitSet::operator -=(const FCA::BitSet& a) 
 { 
-    m_bs -= a.m_bs; 
+    mBS -= a.mBS; 
     return *this;
 }
 
-FCA::BitSet &FCA::BitSet::operator &=(const FCA::BitSet &a) 
+FCA::BitSet& FCA::BitSet::operator &=(const FCA::BitSet& a) 
 {
-    m_bs &= a.m_bs;
+    mBS &= a.mBS;
     return *this; 
 }
 
-bool FCA::BitSet::operator ==(const FCA::BitSet &a) const
+size_t FCA::BitSet::findFirst() const
 {
-    return m_bs == a.m_bs; 
+    return mBS.find_first();
 }
 
-bool FCA::BitSet::operator !=(const FCA::BitSet &a) const
+size_t FCA::BitSet::findNext(const size_t pos) const
 {
-    return m_bs != a.m_bs; 
+    return mBS.find_next(pos);
 }
 
-bool FCA::BitSet::operator < (const FCA::BitSet &a) const
+bool FCA::operator ==(const FCA::BitSet& a, const FCA::BitSet& b)
 {
-    return m_bs < a.m_bs;
+    return a.mBS == b.mBS; 
 }
 
-FCA::BitSet FCA::operator -(const FCA::BitSet &a, const FCA::BitSet &b)
+bool FCA::operator !=(const FCA::BitSet& a, const FCA::BitSet& b)
 {
-    BitSet tmp(a);
-    return tmp -= b;
+    return a.mBS != b.mBS; 
 }
 
-FCA::BitSet FCA::operator &(const FCA::BitSet &a, const FCA::BitSet &b)
+bool FCA::operator <(const FCA::BitSet& a, const FCA::BitSet& b)
 {
-    BitSet tmp(a);
-    return tmp &= b;
+    return a.mBS < b.mBS;
+}
+
+bool FCA::operator <=(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return a.mBS <= b.mBS;
+}
+
+bool FCA::operator >(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return a.mBS > b.mBS;
+}
+
+bool FCA::operator >=(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return a.mBS >= b.mBS;
+}
+
+FCA::BitSet FCA::operator &(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return BitSet(a) &= b;
+}
+
+FCA::BitSet FCA::operator |(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return BitSet(a) |= b;
+}
+
+FCA::BitSet FCA::operator ^(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return BitSet(a) ^= b;
+}
+
+FCA::BitSet FCA::operator -(const FCA::BitSet& a, const FCA::BitSet& b)
+{
+    return BitSet(a) -= b;
+}
+
+void FCA::ToString(const FCA::BitSet& a, std::string& s)
+{
+    s.reserve(a.size());
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        if (a.test(i))
+            s += "1";
+        else
+            s += "0";
+    }
+}
+
+void FCA::ToIndList(const FCA::BitSet& a, std::string& s, const std::string& delimiter)
+{
+    std::stringstream ss;
+    const size_t first = a.findFirst();
+    if (FCA::BitSet::npos != first)
+    {
+        ss << first;
+    }
+
+    for (size_t i = a.findNext(first); i != FCA::BitSet::npos; i = a.findNext(i))
+    {
+        ss << delimiter << i;
+    }
+
+    s = ss.str();
 }
 
 # endif //BOOST_BITSET_ON
 
 # ifdef MAGIC_BITSET_ON
 
-FCA::BitSet::BitSet(const size_t new_size) : m_bs(new_size) {}
+FCA::BitSet::BitSet(const size_t new_size) : mBS(new_size) {}
 
-FCA::BitSet::BitSet(const FCA::BitSet &a) : m_bs(a.m_bs) {}
+FCA::BitSet::BitSet(const FCA::BitSet& a) : mBS(a.mBS) {}
 
 bool FCA::BitSet::none() const
 { 
-    return m_bs.none(); 
+    return mBS.none(); 
 }
 
 bool FCA::BitSet::test(const size_t ind) const 
 { 
-    return m_bs.test(ind);
+    return mBS.test(ind);
 }
 
 void FCA::BitSet::set(const size_t ind) 
 {
-    m_bs.set(ind);
+    mBS.set(ind);
 }
 
 void FCA::BitSet::set() 
 { 
-    m_bs.set();
+    mBS.set();
 }
 
 void FCA::BitSet::reset(const size_t ind)
 { 
-    m_bs.set(ind, false);
+    mBS.set(ind, false);
 }
 
 void FCA::BitSet::reset() 
 {
-    m_bs.reset();
+    mBS.reset();
 }
 
 void FCA::BitSet::flip() 
 { 
-    m_bs.flip(); 
+    mBS.flip(); 
 }
 
-bool FCA::BitSet::is_subset_of(const FCA::BitSet &a) const 
+bool FCA::BitSet::is_subset_of(const FCA::BitSet& a) const 
 { 
-    for (size_t i = 0; i < m_bs.size(); ++i)
+    for (size_t i = 0; i < mBS.size(); ++i)
     {
-        if (!a.m_bs.test(i) && m_bs.test(i))
+        if (!a.mBS.test(i)& & mBS.test(i))
         {
             return false;
         }
@@ -167,64 +231,64 @@ bool FCA::BitSet::is_subset_of(const FCA::BitSet &a) const
     return true;
 }
 
-bool FCA::BitSet::is_proper_subset_of(const FCA::BitSet &a) const
+bool FCA::BitSet::is_proper_subset_of(const FCA::BitSet& a) const
 { 
-    return is_subset_of(a) && m_bs != a.m_bs;
+    return is_subset_of(a)& & mBS != a.mBS;
 }
 
 size_t FCA::BitSet::size() const 
 { 
-    return m_bs.size();
+    return mBS.size();
 }
 
 void FCA::BitSet::resize(const size_t new_size)
 {
-    return m_bs.resize(new_size);
+    return mBS.resize(new_size);
 }
 
 size_t FCA::BitSet::count() const 
 { 
-    return m_bs.count(); 
+    return mBS.count(); 
 }
 
-FCA::BitSet &FCA::BitSet::operator |=(const FCA::BitSet &a) 
+FCA::BitSet& FCA::BitSet::operator |=(const FCA::BitSet& a) 
 { 
-    m_bs |= a.m_bs; 
+    mBS |= a.mBS; 
     return *this;
 }
 
-FCA::BitSet &FCA::BitSet::operator -=(const FCA::BitSet &a) 
+FCA::BitSet& FCA::BitSet::operator -=(const FCA::BitSet& a) 
 { 
-    m_bs -= a.m_bs; 
+    mBS -= a.mBS; 
     return *this;
 }
 
-FCA::BitSet &FCA::BitSet::operator &=(const FCA::BitSet &a) 
+FCA::BitSet& FCA::BitSet::operator& =(const FCA::BitSet& a) 
 {
-    m_bs &= a.m_bs;
+    mBS& = a.mBS;
     return *this; 
 }
 
-bool FCA::BitSet::operator ==(const FCA::BitSet &a) 
+bool FCA::BitSet::operator ==(const FCA::BitSet& a) 
 {
-    return m_bs == a.m_bs; 
+    return mBS == a.mBS; 
 }
 
-bool FCA::BitSet::operator !=(const FCA::BitSet &a) 
+bool FCA::BitSet::operator !=(const FCA::BitSet& a) 
 {
-    return m_bs != a.m_bs; 
+    return mBS != a.mBS; 
 }
 
-FCA::BitSet FCA::operator -(const FCA::BitSet &a, const FCA::BitSet &b)
+FCA::BitSet FCA::operator -(const FCA::BitSet& a, const FCA::BitSet& b)
 {
     BitSet tmp(a);
     return tmp -= b;
 }
 
-FCA::BitSet FCA::operator &(const FCA::BitSet &a, const FCA::BitSet &b)
+FCA::BitSet FCA::operator& (const FCA::BitSet& a, const FCA::BitSet& b)
 {
     BitSet tmp(a);
-    return tmp &= b;
+    return tmp& = b;
 }
 
 # endif //MAGIC_BITSET_ON
--- CanonicalBasis/fca_bitset.h
+++ CanonicalBasis/fca_bitset.h
@@ -5,6 +5,9 @@
 
 # include "fca_config.h"
 
+# include <string>
+# include <climits>
+
 # ifdef BOOST_BITSET_ON
 # include <boost/dynamic_bitset.hpp>
 # endif //BOOST_BITSET_ON
@@ -18,7 +21,11 @@ namespace FCA
     class BitSet
     {
     public:
-        BitSet(const size_t new_size = 0);
+# ifdef BOOST_BITSET_ON
+        static const size_t npos = UINT_MAX;
+# endif //BOOST_BITSET_ON
+
+        BitSet(const size_t newSize = 0);
         BitSet(const BitSet &a);
 
         bool none() const;
@@ -31,34 +38,46 @@ namespace FCA
 
         void flip();
 
-        bool is_subset_of(const BitSet &a) const;
-        bool is_proper_subset_of(const BitSet &a) const;
+        bool is_subset_of(const BitSet& a) const;
+        bool is_proper_subset_of(const BitSet& a) const;
 
         size_t size() const;
         void resize(const size_t new_size);
 
         size_t count() const;
 
-        BitSet &operator |=(const BitSet &a);
-        BitSet &operator -=(const BitSet &a);
-        BitSet &operator &=(const BitSet &a);
+        BitSet &operator &=(const BitSet& a);
+        BitSet &operator |=(const BitSet& a);
+        BitSet &operator ^=(const BitSet& a);
+        BitSet &operator -=(const BitSet& a);
 
-        bool operator ==(const BitSet &a) const;
-        bool operator !=(const BitSet &a) const;
-        bool operator <(const BitSet &a) const;
+        size_t findFirst() const;
+        size_t findNext(const size_t pos) const;
+
+        friend bool operator ==(const BitSet& a, const BitSet& b);
+        friend bool operator !=(const BitSet& a, const BitSet& b);
+        friend bool operator <(const BitSet& a, const BitSet& b);
+        friend bool operator <=(const BitSet& a, const BitSet& b);
+        friend bool operator >(const BitSet& a, const BitSet& b);
+        friend bool operator >=(const BitSet& a, const BitSet& b);
 
     private:
 # ifdef BOOST_BITSET_ON
-        boost::dynamic_bitset<unsigned long> m_bs;
+        boost::dynamic_bitset<unsigned long> mBS;
 # endif //BOOST_BITSET_ON
 
 # ifdef MAGIC_BITSET_ON
-        bm::bvector<> m_bs;
+        bm::bvector<> mBS;
 # endif //MAGIC_BITSET_ON
     };
 
-    BitSet operator -(const BitSet &a, const BitSet &b);
-    BitSet operator &(const BitSet &a, const BitSet &b);
+    BitSet operator &(const BitSet& a, const BitSet& b);
+    BitSet operator |(const BitSet& a, const BitSet& b);
+    BitSet operator ^(const BitSet& a, const BitSet& b);
+    BitSet operator -(const BitSet& a, const BitSet& b);
+
+    void ToString(const BitSet& a, std::string& s);
+    void ToIndList(const BitSet& a, std::string& s, const std::string& delimiter = " ");
 };
 
 # endif //FCA_BITSET_H_
