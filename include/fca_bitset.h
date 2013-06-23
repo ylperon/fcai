7b4d6d5d7cb1f0cb9366f46cdca14a5b8c040127
Kostya Bazhanov <kostyabazhanov@mail.ru>
2013-06-23 15:55:50 +0400

    BasicBitSet constructor and destructor

--- include/fca_bitset.h
+++ include/fca_bitset.h
@@ -14,8 +14,9 @@ namespace FCA
         static const size_t BitsPerBlock = BlockSize * 8;
         static const size_t npos = std::numeric_limits<size_t>::max();
 
-        BasicBitSet(const size_t length = 0);
+        explicit BasicBitSet(const size_t length = 0);
         BasicBitSet(const BasicBitSet &a);
+        virtual ~BasicBitSet();
 
         bool any() const;
         bool none() const;
@@ -27,6 +28,7 @@ namespace FCA
         void reset();
         void flip(const size_t ind);
         void flip();
+        BasicBitSet operator ~() const;
 
         bool is_subset_of(const BasicBitSet& a) const;
         bool is_proper_subset_of(const BasicBitSet& a) const;
@@ -37,15 +39,17 @@ namespace FCA
         size_t count() const;
         size_t count_zeros() const
 
-        BasicBitSet &operator &=(const BasicBitSet& a);
-        BasicBitSet &operator |=(const BasicBitSet& a);
-        BasicBitSet &operator ^=(const BasicBitSet& a);
-        BasicBitSet &operator -=(const BasicBitSet& a);
+        BasicBitSet& operator &=(const BasicBitSet& a);
+        BasicBitSet& operator |=(const BasicBitSet& a);
+        BasicBitSet& operator ^=(const BasicBitSet& a);
+        BasicBitSet& operator -=(const BasicBitSet& a);
 
         size_t findFirst() const;
         size_t findNext(const size_t pos) const;
 
-        void swap(BasicBitset& a);
+        void swap(BasicBitSet& a);
+
+        BasicBitSet& operator =(const BasicBitSet& a);
 
         friend bool operator ==(const BasicBitSet& a, const BasicBitSet& b);
         friend bool operator !=(const BasicBitSet& a, const BasicBitSet& b);
@@ -76,3 +80,55 @@ namespace FCA
 
     typedef BasicBitSet<unsigned long> BitSet;
 };
+
+/* Realization of templates */
+
+#include <ctsring>
+
+namespace {
+    template <typename Block>
+    inline size_t BlocksReqired(const size_t length) {
+        return (length + BasicBitSet<Block>::BitsPerBlock - 1) / BasicBitSet<Block>::BitsPerBlock;
+    }
+
+    tempalte <typename Block>
+    inline size_t BlockInd(const size_t ind) {
+        return ind / BasicBitSet<Block>::BitsPerBlock;
+    }
+
+    template <typename Block>
+    inline size_t BitIndInBlock(const size_t ind) {
+        return ind % BasicBitSet<Block>::BitsPerBlock;
+    }
+}
+
+template<typename Block>
+BasicBitSet<Block>::BasicBitSet(const size_t length) {
+    if (0 == length) {
+        a = 0;
+        aSize = 0;
+        this->length = 0;
+        return;
+    }
+
+    aSize = BlocksReqired<Block>(length);
+    a = new Block[aSize]();
+    this->length = length();
+}
+
+template <typename Block>
+BasicBitSet<Block>::BasicBitSet(const BasicBitSet<Block>& a) {
+    aSize = a.aSize;
+    length = a.length;
+    if (0 != aSize) {
+        a = new Block[aSize];
+        memcpy(a, a.a, aSize * BlockSize;
+    }
+}
+
+template <typename Block>
+BasicBitSet<Block>::~BasicBitSet() {
+    if (0 != a) {
+        delete [] a;
+    }
+}
