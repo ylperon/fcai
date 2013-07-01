a322dd0188b694abd5020bf9a245d7593a9e3d17
Kostya Bazhanov <kostyabazhanov@mail.ru>
2013-07-01 23:34:11 +0400

    Forgotten header

--- tests/test_bitset.h
+++ tests/test_bitset.h
@@ -6,7 +6,11 @@
 #include "test_util.h"
 #include "fca_bitset.h"
 
+extern TestFunctionGroup TestBitSetAll;
+
 std::vector<TestFunction> GetAllBitSetTestFunctions();
+
+TEST_RESULT TestBitSet(const std::string& indent, size_t& ok, size_t& fail);
 
 TEST_RESULT TestBitSetConctruct();
 TEST_RESULT TestBitSetCopyConstruct();
